1e68f533696e372f8da780b1a182ca59f01ab947
Nguyen Tran <ntran272@csu.fullerton.edu>
2017-10-12 19:16:59 -0700

    Add ThankyouPage

--- LawProject/QuestionStore/public/QuestionForm/scripts/main.js
+++ LawProject/QuestionStore/public/QuestionForm/scripts/main.js
@@ -9,26 +9,26 @@
   var RemoteDataStore = App.RemoteDataStore;
   var FormHandler = App.FormHandler;
   var Validation = App.Validation;
-  var CheckList = App.CheckList;
+  //var CheckList = App.CheckList;
   var remoteDS = new RemoteDataStore(SERVER_URL);
   var webshim = window.webshim;
   var myStore = new Store('ncc-1701', remoteDS);
   window.myStore = myStore;
 
-  CheckList.prototype.addClickHandler = function (fn){
-    this.$element.on('click', 'input', function(event){
-      var email = event.target.value;
-      this.removeRow(email);
-      fn(email);
-    }.bind(this));
-  };
+  // CheckList.prototype.addClickHandler = function (fn){
+  //   this.$element.on('click', 'input', function(event){
+  //     var email = event.target.value;
+  //     this.removeRow(email);
+  //     fn(email);
+  //   }.bind(this));
+  // };
 
-  var checkList = new CheckList(CHECKLIST_SELECTOR);
-  checkList.addClickHandler(myStore.deliverOrder.bind(myStore));
+  //var checkList = new CheckList(CHECKLIST_SELECTOR);
+  //checkList.addClickHandler(myStore.deliverOrder.bind(myStore));
   var formHandler = new FormHandler(FORM_SELECTOR);
   formHandler.addSubmitHandler(function(data) {
     myStore.createOrder.call(myStore, data);
-    checkList.addRow.call(checkList, data);
+  //  checkList.addRow.call(checkList, data);
   });
 
   formHandler.addInputHandler(Validation.isCompanyEmail);
