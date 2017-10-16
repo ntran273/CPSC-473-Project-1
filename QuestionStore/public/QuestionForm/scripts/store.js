75f77880233cfb4192d34b1fe8bf16a9632e8daa
Nguyen Tran <ntran272@csu.fullerton.edu>
2017-10-15 22:56:32 -0700

    Beaufity

--- QuestionStore/public/QuestionForm/scripts/store.js
+++ QuestionStore/public/QuestionForm/scripts/store.js
@@ -8,22 +8,22 @@
   }
 
   //Create Order
-  Store.prototype.createOrder = function (order) {
+  Store.prototype.createOrder = function(order) {
     console.log('Adding order for ' + order.emailAddress);
     this.db.add(order.emailAddress, order);
   };
 
   //Removing order after Deliver
-  Store.prototype.deliverOrder = function (customerId) {
+  Store.prototype.deliverOrder = function(customerId) {
     console.log('Delivering order for ' + customerId);
     this.db.remove(customerId);
   };
 
-  Store.prototype.printOrders = function () {
+  Store.prototype.printOrders = function() {
     var customerIdArray = Object.keys(this.db.getAll());
 
     console.log('Store #' + this.storeId + ' has pending orders:');
-    customerIdArray.forEach(function (id) {
+    customerIdArray.forEach(function(id) {
       console.log(this.db.get(id));
     }.bind(this));
   };
--- QuestionStore/public/login/scripts/main.js
+++ QuestionStore/public/login/scripts/main.js
@@ -9,12 +9,12 @@
   var remoteDS = new RemoteDataStore(SERVER_URL);
   var checkList = new CheckList(CHECKLIST_SELECTOR, remoteDS);
   var customerArray = checkList.getArray();
-  for(var i = 0; i< customerArray.length; i++){
+  for (var i = 0; i < customerArray.length; i++) {
     checkList.addRow(customerArray[i]);
     checkList.addClickHandlers(i);
   }
   //close button handler
-  document.getElementById("close").addEventListener("click",function(){
+  document.getElementById("close").addEventListener("click", function() {
     document.getElementById("questionDetails").style.visibility = "hidden";
   });
 })(window);
